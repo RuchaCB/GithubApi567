83ea1fb81c4c1ba3e8715ef7c07c372fcd256d04
RuchaCB <36187469+RuchaCB@users.noreply.github.com>
2020-02-25 23:35:04 -0500

    Update GitHubApi567Test.py

--- GitHubApi567Test.py
+++ GitHubApi567Test.py
@@ -1,24 +1,16 @@
 import unittest
 from unittest import mock
-import GitHubApi567
+import GitHubApi567 
 
 
 class TestGitHubApi567(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
     @mock.patch('GitHubApi567.u_commits')
     def testu_commits(self, mock_u_com): 
-        total = 0
-        l_1=[]
-        for repo, num in mock_u_com.return_value('richkempinski'):
-            print(repo, num)
-            l_1.append("Repo: %s Number of commits: %d" % (repo, num))
-            total += num 
-        l_1.append("Done")
-        self.assertEqual(l_1,["Repo: helloworld Number of commits: 6", "Repo: Mocks Number of commits: 10","Repo: Project1 Number of commits: 2","Repo: threads-of-life Number of commits: 1","Done"])
-    
-    """@mock.patch('GitHubApi567.u_commits')
-    def mock_u_commits(self, mock_u_com):
-        print(mock_u_com)"""
+        mock_u_com.return_value = ["Repo: helloworld Number of commits: 6", "Repo: Mocks Number of commits: 10","Repo: Project1 Number of commits: 2","Repo: threads-of-life Number of commits: 1","Done"]
+        expected = ["Repo: helloworld Number of commits: 6", "Repo: Mocks Number of commits: 10","Repo: Project1 Number of commits: 2","Repo: threads-of-life Number of commits: 1","Done"]
+        gihub_id = "richkempinski"
+        self.assertEqual(GitHubApi567.u_commits(gihub_id), expected)
 
 if __name__ == '__main__':
     print('Running unit tests')
