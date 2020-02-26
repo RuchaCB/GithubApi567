6fe10011fdbc2e469aa0015b972bdf5805bfa764
RuchaCB <36187469+RuchaCB@users.noreply.github.com>
2020-02-25 23:01:46 -0500

    Update GitHubApi567Test.py

--- GitHubApi567Test.py
+++ GitHubApi567Test.py
@@ -16,7 +16,7 @@ class TestGitHubApi567(unittest.TestCase):
         self.assertEqual(l_1,["Repo: helloworld Number of commits: 6", "Repo: Mocks Number of commits: 10","Repo: Project1 Number of commits: 2","Repo: threads-of-life Number of commits: 1","Done"])
     
     @mock.patch('u_commits')
-    def mock_u_commits(mock_u_com):
+    def mock_u_commits(self, mock_u_com):
         print(mock_u_com)
 
 if __name__ == '__main__':
